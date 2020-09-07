f9aa5e1b294efc8eb9a8c7fdabce698e7b1d94da
skeleton1231 <814010079@qq.com>
2020-09-07 16:15:14 +0800

    fix

--- components/BrandComponent.php
+++ components/BrandComponent.php
@@ -1029,10 +1029,15 @@ Class BrandComponent extends Component
 
        // $good['title'] = preg_replace('#[\x{4e00}-\x{9fa5}]#u', ' ', $good['title']);
        // $good['title'] = preg_replace('/([\x80-\xff]*)/i', ' ', $good['title']);
-        preg_match_all('^[A-Za-z0-9]+$', $good['title'], $matches);
+        if(preg_match_all('/\w+/', $good['title'], $matches)){
 
-        $title = implode(' ', $matches);
+            $title = implode(' ', $matches);
 
+        }
+        else{
+
+            $title = '';
+        }
 
         $code = substr($good['shop_id'] . '/' . $good['goods_id'], -6);
 
